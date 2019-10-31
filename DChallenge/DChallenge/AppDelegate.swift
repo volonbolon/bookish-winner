8e0b9dbda8d7ad74c6b69690255d7a5306f136f7
Ariel Rodriguez <ariel@volonbolon.me>
2019-10-31 10:13:42 -0300

    Add Xcode Project

---
+++ DChallenge/DChallenge/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  DChallenge
+//
+//  Created by Ariel Rodriguez on 31/10/2019.
+//  Copyright © 2019 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration { // swiftlint:disable:this line_length
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
---
+++ DChallenge/DChallenge/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  DChallenge
+//
+//  Created by Ariel Rodriguez on 31/10/2019.
+//  Copyright © 2019 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
