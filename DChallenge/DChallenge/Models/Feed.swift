d88a702017595649a21b188ce85248a0e49f7dbf
Ariel Rodriguez <ariel@volonbolon.me>
2019-10-31 13:51:33 -0300

    Add models

---
+++ DChallenge/DChallenge/Models/Feed.swift
@@ -0,0 +1,15 @@
+//
+//  Feed.swift
+//  DChallenge
+//
+//  Created by Ariel Rodriguez on 31/10/2019.
+//  Copyright © 2019 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Feed: Codable {
+    let id: Int
+    let title: String
+    let url: String
+}
