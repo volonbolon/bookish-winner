be90146fd5e6685dda4ef08ecde3eff2d2e9a60e
Ariel Rodriguez <ariel@volonbolon.me>
2019-10-31 14:01:30 -0300

    Add cells

---
+++ DChallenge/DChallenge/View Controllers/FeedsTableDatasource.swift
@@ -0,0 +1,22 @@
+//
+//  FeedsTableDatasource.swift
+//  DChallenge
+//
+//  Created by Ariel Rodriguez on 31/10/2019.
+//  Copyright © 2019 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FeedsDatasource: NSObject, UITableViewDataSource {
+    var feeds: [Feed] = []
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feeds.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FeedTableViewCell.identifier, for: indexPath)
+        return cell
+    }
+}
---
+++ DChallenge/DChallenge/Views/FeedTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FeedTableViewCell.swift
+//  DChallenge
+//
+//  Created by Ariel Rodriguez on 31/10/2019.
+//  Copyright © 2019 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+    static let identifier = "FeedTableViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
